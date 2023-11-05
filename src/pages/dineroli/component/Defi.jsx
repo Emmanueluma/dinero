8e377298b7d1ee089e8e9b196c7403661145ad2f
unknown <Emmanueluma>
2023-11-05 12:44:05 -0800

    updated the links on dinero library

--- src/pages/dineroli/component/Defi.jsx
+++ src/pages/dineroli/component/Defi.jsx
@@ -13,28 +13,28 @@ const Defi = () => {
       <div className="content">
       <h1>2.0 DEFI</h1>
         <article>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.1 What is DeF</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.2 Advantages and disadvantages of DeFi</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.3 Creating and funding your first self custody web3 wallet</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.4 Transferring funds, bridging, trading and operating on chain</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.5 Lending/Borrowing, leverage and stablecoins</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.6 What are DEXs?</a> 
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.7 Liquidity pools and automated market makers (AMMs)</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.8 Yield Farming and Liquidity Provision</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.9 Stablecoins and Asset Management</a> 
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.10 DeFi yield aggregators</a> 
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.11 Security and Risks</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.12 Smart contracts</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.13 Risks associated with smart contracts</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.14 DeFi hacks and vulnerabilities</a> 
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.15 DeFi tools</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.16 How to use Dexscreener</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.17 How to use Pancake Swap</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.18 How to use Trust wallet</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.19 Risk Management</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.20 Understanding tokenomics</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.21 Finding alpha and understanding the space</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">2.22 Airdrop hunting</a>
+            <a href="https://www.coinbase.com/learn/crypto-basics/what-is-defi" target="_blank" rel="noopener noreferrer">2.1 What is DeF</a>
+            <a href="https://www.theblock.co/learn/245712/what-are-the-advantages-and-disadvantages-of-defi" target="_blank" rel="noopener noreferrer">2.2 Advantages and disadvantages of DeFi</a>
+            <a href="https://youtu.be/nQ3BZVIMjWk?si=_shko4WCxl5GnZan" target="_blank" rel="noopener noreferrer">2.3 Creating and funding your first self custody web3 wallet</a>
+            <a href="https://www.coindesk.com/learn/what-are-blockchain-bridges-and-how-do-they-work/" target="_blank" rel="noopener noreferrer">2.4 Transferring funds, bridging, trading and operating on chain</a>
+            <a href="https://academy.binance.com/en/articles/what-is-leverage-in-crypto-trading" target="_blank" rel="noopener noreferrer">2.5 Lending/Borrowing, leverage and stablecoins</a>
+            <a href="https://youtu.be/2tTVJL4bpTU?si=3mG6HEqLFgvz3oMu" target="_blank" rel="noopener noreferrer">2.6 What are DEXs?</a> 
+            <a href="https://academy.binance.com/en/articles/what-are-liquidity-pools-in-defi" target="_blank" rel="noopener noreferrer">2.7 Liquidity pools and automated market makers (AMMs)</a>
+            <a href="https://academy.binance.com/en/articles/what-is-yield-farming-in-decentralized-finance-defi" target="_blank" rel="noopener noreferrer">2.8 Yield Farming and Liquidity Provision</a>
+            <a href="https://academy.binance.com/en/articles/what-is-a-stablecoin" target="_blank" rel="noopener noreferrer">2.9 Stablecoins and Asset Management</a> 
+            <a href="https://www.alchemy.com/best/defi-yield-aggregators" target="_blank" rel="noopener noreferrer">2.10 DeFi yield aggregators</a> 
+            <a href="https://twitter.com/RubiksWeb3hub/status/1629180007649054722?t=iZPnN_shR27qtqGqrYrSJQ&s=19" target="_blank" rel="noopener noreferrer">2.11 Security and Risks</a>
+            <a href="https://academy.binance.com/en/articles/what-are-smart-contracts" target="_blank" rel="noopener noreferrer">2.12 Smart contracts</a>
+            <a href="https://academy.binance.com/en/articles/what-are-common-bridge-security-vulnerabilities" target="_blank" rel="noopener noreferrer">2.13 Risks associated with smart contracts</a>
+            <a href="https://hacken.io/discover/defi-security/" target="_blank" rel="noopener noreferrer">2.14 DeFi hacks and vulnerabilities</a> 
+            <a href="https://twitter.com/alpha_pls/status/1619325494025125888?t=xpNRgvw5o9tVD0-_eVZcZg&s=19" target="_blank" rel="noopener noreferrer">2.15 DeFi tools</a>
+            <a href="https://twitter.com/DeFiMinty/status/1674810647832297472?t=qdCEyByPe0371SGBChAvKw&s=19" target="_blank" rel="noopener noreferrer">2.16 How to use Dexscreener</a>
+            <a href="https://twitter.com/DeFiMinty/status/1674810647832297472?t=qdCEyByPe0371SGBChAvKw&s=19" target="_blank" rel="noopener noreferrer">2.17 How to use Pancake Swap</a>
+            <a href="https://youtu.be/saXustx90Z0?si=UPWFjqMcwjJLoqQX" target="_blank" rel="noopener noreferrer">2.18 How to use Trust wallet</a>
+            <a href="https://academy.binance.com/en/articles/a-beginners-guide-to-understanding-risk-management" target="_blank" rel="noopener noreferrer">2.19 Risk Management</a>
+            <a href="https://twitter.com/RubiksWeb3hub/status/1629037940008534019?t=HKNENzFllfTPWtBpyLMh2g&s=19" target="_blank" rel="noopener noreferrer">2.20 Understanding tokenomics</a>
+            <a href="https://x.com/cryptopothu/status/1547430699283582977?s=46&t=h8uROEBzbnP6Y6iA_M8-bQ" target="_blank" rel="noopener noreferrer">2.21 Finding alpha and understanding the space</a>
+            <a href="https://twitter.com/zayzay0_/status/1684646188270735360?t=tEgzoCS2wnbu4RK34D-A9g&s=19" target="_blank" rel="noopener noreferrer">2.22 Airdrop hunting</a>
         </article>
 
     </div>
--- src/pages/dineroli/component/Original.jsx
+++ src/pages/dineroli/component/Original.jsx
@@ -13,24 +13,24 @@ const Original = () => {
       <div className="content">
       <h1>4.0 ORDINALS</h1>
         <article>    
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.1 Bitcoin Whitepaper</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.2 Ordinals Ecosytem Overview</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.3 Ordinals most used Tools & Marketplaces</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.4 What are Ordinals</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.5 Getting a taproot wallet</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.6 Inscribing your first ordinal</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.7 Trading your first ordinal</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.8 Satoshis Rarity</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.9 BRC-20 Tokens</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.10 Recursive Inscriptions</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.11 Cursed Inscriptions</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.12 Sat Hunting</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.13 Using ord wallet receive to receive sats</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.14 Creating inscriptions with ord wallet inscribe</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.15 Sending inscriptions with ord wallet send</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.16 Receiving inscriptions with ord wallet receive</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.17 Ordinal Rarity</a>
-            <a href="http://" target="_blank" rel="noopener noreferrer">4.18 Special Sats</a>
+            <a href="https://bitcoin.org/bitcoin.pdf" target="_blank" rel="noopener noreferrer">4.1 Bitcoin Whitepaper</a>
+            <a href="https://docs.ordinals.com/overview.html" target="_blank" rel="noopener noreferrer">4.2 Ordinals Ecosytem Overview</a>
+            <a href="https://x.com/swapski/status/1645485402105561088?s=46" target="_blank" rel="noopener noreferrer">4.3 Ordinals Marketplaces</a>
+            <a href="https://docs.ordinals.com/inscriptions.html" target="_blank" rel="noopener noreferrer">4.4 What are Inscriptions</a>
+            <a href="https://academy.binance.com/en/articles/what-are-ordinals-an-overview-of-bitcoin-nfts" target="_blank" rel="noopener noreferrer">4.5 What Are Ordinals</a>
+            <a href="https://x.com/beb8p/status/1671400063396880386?s=46" target="_blank" rel="noopener noreferrer">4.6 Getting A Taproot Wallet (xVerse)</a>
+            <a href="https://x.com/cryptocooldude/status/1657569434331148288?s=46" target="_blank" rel="noopener noreferrer">4.7 Getting A Taproot Wallet (Unisat)</a>
+            <a href="https://x.com/cryptocooldude/status/1657569434331148288?s=46" target="_blank" rel="noopener noreferrer">4.8 How to inscribe .Sats name </a>
+            <a href="https://x.com/meonbtc/status/1691834855317713375?s=46" target="_blank" rel="noopener noreferrer">4.9 Inscribing Your First Ordinal using ME </a>
+            <a href="https://x.com/waleswoosh/status/1660601584538079234?s=46" target="_blank" rel="noopener noreferrer">4.10 Trading Your First Ordinal</a>
+            <a href="https://x.com/const_quary/status/1674040726777151488?s=46" target="_blank" rel="noopener noreferrer">4.11 Satoshis Rarity</a>
+            <a href="https://x.com/jiurn/status/1640377171800911872?s=46" target="_blank" rel="noopener noreferrer">4.12 BRC-20 Tokens</a>
+            <a href="https://x.com/nealinnft/status/1719007709930848503?s=46" target="_blank" rel="noopener noreferrer">4.13 Recursive Inscriptions</a>
+            <a href="https://x.com/billyrestey/status/1661406587511271424?s=46" target="_blank" rel="noopener noreferrer">4.14 Cursed Inscriptions</a>
+            <a href="https://x.com/itsfranken/status/1671807110244610048?s=46" target="_blank" rel="noopener noreferrer">4.15 UTXOs</a>
+            <a href="https://x.com/itsfranken/status/1639202586074152969?s=46" target="_blank" rel="noopener noreferrer">4.16 Sat Hunting</a>
+            <a href="https://x.com/buxors/status/1626828300168626178?s=46" target="_blank" rel="noopener noreferrer">4.17 How to find your Ordinal Rarity</a>
+            <a href="https://blog.gamma.io/special-sats-and-ordinals#:~:text=Rare%20sats%3A%20The%20first%20sat,residing%20in%20the%20genesis%20block" target="_blank" rel="noopener noreferrer">4.18 Special Sats</a>
         </article>
     </div>
     </div>
